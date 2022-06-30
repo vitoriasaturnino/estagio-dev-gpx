377d1253b030c644d116b7728da65d95eb8d744c
Vitória Moura <vim.saturnino@gmail.com>
2022-06-30 16:23:33 -0300

    relógio inserido no layout principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
+import Relogio from './components/Relogio';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div id="test_alinhamento">
-          <div className="row_1">
-            <p>Relogio</p>
+          <div className="relogio">
+          <Relogio/>
           </div>
           <div className="row_2">
             <p>Manipulacao de items</p>

66fc83dce49257d48f075b049d9438a4df750eb0
Vitória Moura <vim.saturnino@gmail.com>
2022-06-30 16:17:06 -0300

    Configuração do layout inicial do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="test_alinhamento">
+          <div className="row_1">
+            <p>Relogio</p>
+          </div>
+          <div className="row_2">
+            <p>Manipulacao de items</p>
+          </div>
+        </div>
+        <div className="column">
+          <p>Bola timida</p>
+        </div>
       </header>
     </div>
   );
